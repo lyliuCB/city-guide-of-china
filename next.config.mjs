85797ca618c5463f9ac6dac16b88d57636167bd6
lyliuCB <lynn.liu@careerbuilder.com>
2024-05-28 17:44:44 +0800

    deploy to github pages

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+    output: "export",
+    basePath: "/city-guide-of-china",
+    images: {
+      unoptimized: true,
+    },
+};
+
+export default nextConfig;
